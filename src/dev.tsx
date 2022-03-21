b2f114e2acc3c42bb26c102ff89cadf72d2218c1
Mat Jordan <mat@northwestern.edu>
2022-03-21 14:27:02 -0400

    Add @iiif/vault.

--- src/dev.tsx
+++ src/dev.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./index";
 
-let sampleCollection: string = `http://127.0.0.1:8080/fixtures/iiif/collections/sample.json`;
+let sampleCollection: string = `http://127.0.0.1:8080/fixtures/iiif/collection/nez-perce.json`;
 
 ReactDOM.render(
   <App collectionId={sampleCollection} />,
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Vault } from "@iiif/vault";
 interface Props {
   collectionId: string;
 }
 
 const App: React.FC<Props> = ({ collectionId }) => {
+  useEffect(() => {
+    /**
+     * Loaded manifest and site using @hyperion-framework/vault.
+     */
+    const vault = new Vault();
+
+    vault
+      .loadCollection(collectionId)
+      .then((data: any) => {
+        console.log(data);
+      })
+      .catch((error: any) => {
+        console.error(`Manifest failed to load: ${error}`);
+      });
+  }, []);
+
   return <>{collectionId}</>;
 };
 
