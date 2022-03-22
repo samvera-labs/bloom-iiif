45c729c92106fe0748a45d7a634524298f3b987c
Mat Jordan <mat@northwestern.edu>
2022-03-21 22:04:10 -0400

    Add collection fixtures.

--- src/dev.tsx
+++ src/dev.tsx
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 import App from "./index";
 
 let sampleCollectionA: string = `http://127.0.0.1:8080/fixtures/iiif/collection/nez-perce.json`;
-let sampleCollectionB: string = `http://127.0.0.1:8080/fixtures/iiif/collection/nez-perce.json`;
-let sampleCollectionC: string = `http://127.0.0.1:8080/fixtures/iiif/collection/nez-perce.json`;
+let sampleCollectionB: string = `http://127.0.0.1:8080/fixtures/iiif/collection/joseph.json`;
+let sampleCollectionC: string = `http://127.0.0.1:8080/fixtures/iiif/collection/tsawatenok.json`;
 
 ReactDOM.render(
   <>
