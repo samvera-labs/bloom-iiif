9b360e6063007733bd26e5ad382853ca4e3741a0
Mat Jordan <mat@northwestern.edu>
2022-03-23 08:12:51 -0400

    Adjust figure child component props.

--- src/components/Figure/Figure.tsx
+++ src/components/Figure/Figure.tsx
@@ -1,18 +1,15 @@
 import React from "react";
-import { useGetLabel } from "hooks/useGetLabel";
-import { Collection, Manifest } from "@iiif/presentation-3";
 import { Description, FigureStyled, Image, Title } from "./Figure.styled";
 
 interface FigureProps {
-  item: Collection | Manifest;
+  caption: string;
 }
 
-const Figure: React.FC<FigureProps> = ({ item }) => {
-  const { label } = item;
+const Figure: React.FC<FigureProps> = ({ caption }) => {
   return (
     <FigureStyled>
       <Image style={{ width: "100%", height: `120px` }} />
-      <Title>{useGetLabel(label)}</Title>
+      <Title>{caption}</Title>
       <Description>Image</Description>
     </FigureStyled>
   );
--- src/components/Items/Item.tsx
+++ src/components/Items/Item.tsx
@@ -1,18 +1,14 @@
 import React from "react";
 import Figure from "components/Figure/Figure";
-import { CollectionItems, Collection, Manifest } from "@iiif/presentation-3";
-import { ItemsStyled } from "./Items.styled";
+import { Collection, Manifest } from "@iiif/presentation-3";
+import { useGetLabel } from "hooks/useGetLabel";
 
 interface ItemProps {
   item: Collection | Manifest;
 }
 
 const Item: React.FC<ItemProps> = ({ item }) => {
-  return (
-    <>
-      <Figure item={item} />
-    </>
-  );
+  return <Figure caption={useGetLabel(item.label)} />;
 };
 
 export default Item;
