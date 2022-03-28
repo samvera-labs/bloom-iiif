33f892c45a4c3b40370677955f7fda3dc82d25ea
Mat Jordan <mat@northwestern.edu>
2022-03-28 14:23:44 -0400

    Detect active focus/hover for manifest previewing.

--- src/components/Items/Item.tsx
+++ src/components/Items/Item.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Figure from "components/Figure/Figure";
 import { Collection, Manifest } from "@iiif/presentation-3";
 import { useGetLabel } from "hooks/useGetLabel";
@@ -11,10 +11,24 @@ interface ItemProps {
 }
 
 const Item: React.FC<ItemProps> = ({ item }) => {
+  const [focused, setFocused] = useState<boolean>(false);
+  const [preview, setPreview] = useState<boolean>(false);
   const store = useCollectionState();
   const { vault } = store;
 
-  useEffect(() => {}, []);
+  const itemRef = useRef(null);
+
+  useEffect(() => {
+    focused && setTimeout(() => setPreview(true), 1000);
+  }, [focused]);
+
+  useEffect(() => {}, [preview]);
+
+  const onFocus = () => setFocused(true);
+  const onBlur = () => {
+    setFocused(false);
+    setPreview(false);
+  };
 
   /**
    * todo: be more defensive about collections without `thumbnail`
@@ -31,7 +45,16 @@ const Item: React.FC<ItemProps> = ({ item }) => {
 
   return (
     <ItemStyled>
-      <Anchor href={url}>
+      {preview && focused && <div></div>}
+      <Anchor
+        href={url}
+        tabIndex={0}
+        onFocus={onFocus}
+        onBlur={onBlur}
+        onMouseOver={onFocus}
+        onMouseOut={onBlur}
+        ref={itemRef}
+      >
         <Figure
           caption={useGetLabel(item.label)}
           description={useGetLabel(item.summary)}
