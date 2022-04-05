c951ca37ea1dfe55f71cd35a9fc27f5b21b186db
Mat Jordan <mat@northwestern.edu>
2022-04-05 10:16:03 -0400

    Add modern resize functionality on `<Items/>` controls

--- src/components/Figure/Figure.tsx
+++ src/components/Figure/Figure.tsx
@@ -6,15 +6,18 @@ import {
   Image,
   Placeholder,
   Title,
+  Width,
 } from "./Figure.styled";
 import * as AspectRatio from "@radix-ui/react-aspect-ratio";
 import { ContentResource } from "@iiif/presentation-3";
 import Video from "./Video";
+import { useCollectionDispatch } from "context/collection-context";
 
 interface FigureProps {
   caption: string;
   description: string;
   image: string | null;
+  index: number;
   video: ContentResource | null;
   isFocused: boolean;
 }
@@ -23,11 +26,14 @@ const Figure: React.FC<FigureProps> = ({
   caption,
   description,
   image,
+  index,
   isFocused,
   video,
 }) => {
+  const dispatch: any = useCollectionDispatch();
   const [loaded, setLoaded] = useState(false);
   const imgRef = useRef<HTMLImageElement>(null);
+  const widthRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     if (imgRef.current && imgRef.current.complete) setLoaded(true);
@@ -37,9 +43,33 @@ const Figure: React.FC<FigureProps> = ({
     setLoaded(false);
   }, [image]);
 
+  useEffect(() => {
+    const resizeObserver = new ResizeObserver(
+      (entries: ResizeObserverEntry[]) => {
+        for (let entry of entries) {
+          if (entry.contentBoxSize) {
+            const contentBoxSize: ResizeObserverSize = Array.isArray(
+              entry.contentBoxSize
+            )
+              ? entry.contentBoxSize[0]
+              : entry.contentBoxSize;
+            dispatch({
+              type: "updateItemHeight",
+              itemHeight: contentBoxSize.inlineSize,
+            });
+          }
+        }
+      }
+    );
+
+    if (index === 0 && widthRef.current)
+      resizeObserver.observe(widthRef.current);
+  }, [index, loaded]);
+
   return (
     <FigureStyled isFocused={isFocused}>
       <AspectRatio.Root ratio={1 / 1}>
+        <Width ref={widthRef} />
         <Placeholder>
           {video && <Video resource={video} isFocused={isFocused} />}
           {image && (
