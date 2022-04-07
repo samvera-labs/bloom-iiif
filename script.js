603c7c5d4ba542a05a709de2c93a8c93e8278569
mathewjordan <mat@northwestern.edu>
2022-04-07 16:25:36 +0000

    Deploying to gh-pages from @ samvera-labs/bloom-iiif@01f269e7dffb99acf0c72e049a7791b79159e079 🚀

--- script.js
+++ script.js
@@ -23873,6 +23873,10 @@ For more info, visit https://reactjs.org/link/mock-scheduler`);
       label: "Flora and Fauna Graeca"
     },
     {
+      url: "https://figgy.princeton.edu/concern/scanned_resources/8b73b1bb-6417-4ae1-b516-bb68d999ed2f/manifest",
+      label: "Clarissa"
+    },
+    {
       url: "https://raw.githubusercontent.com/samvera-labs/bloom-iiif/main/public/fixtures/iiif/collection/athletic-department-footbal-films.json",
       label: "Football Films"
     }
