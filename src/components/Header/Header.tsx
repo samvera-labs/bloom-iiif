95bf768ddf9ce8758a34477cb376e0d616f3e08d
Mat Jordan <mat@northwestern.edu>
2022-05-01 17:18:21 -0400

    Refine Nectar treatment on Header.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,23 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ContentResource, InternationalString } from "@iiif/presentation-3";
 import { HeaderStyled } from "./Header.styled";
 import { Homepage, Label, Summary } from "@samvera/nectar-iiif";
 
 interface HeaderProps {
   label: InternationalString;
-  summary: InternationalString | null;
-  homepage: ContentResource[] | null;
+  summary: InternationalString;
+  homepage: ContentResource[];
 }
 
-const Header: React.FC<HeaderProps> = ({ label, summary, homepage = null }) => {
+const Header: React.FC<HeaderProps> = ({ label, summary, homepage }) => {
+  const [hasHomepage, setHasHomepage] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (homepage.length > 0) setHasHomepage(true);
+  }, [homepage]);
+
   return (
     <HeaderStyled>
-      <Homepage homepage={homepage} className="bloom-header-homepage">
-        <Label label={label} as="span" className="bloom-header-label" />
-      </Homepage>
-      {summary && (
-        <Summary summary={summary} as="span" className="bloom-header-summary" />
+      {hasHomepage ? (
+        <Homepage homepage={homepage}>
+          <Label label={label} as="span" />
+        </Homepage>
+      ) : (
+        <Label label={label} as="span" />
       )}
+
+      {summary && <Summary summary={summary} as="span" />}
     </HeaderStyled>
   );
 };
