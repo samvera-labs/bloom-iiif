b1b5b486893028874bb49afe354c5f7ae4a916e6
Mat Jordan <mat@northwestern.edu>
2022-08-23 13:52:00 -0400

    Fix demo css.

--- utils/html.js
+++ utils/html.js
@@ -8,6 +8,7 @@ async function buildHTML(isStatic) {
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title>Bloom IIIF</title>
       ${isStatic ? ` <link rel="stylesheet" href="./default.css" />` : ``}
+      <link rel="stylesheet" href="./script.css" />
       <link rel="stylesheet" href="./styles.css" />
     </head>
     <body>
