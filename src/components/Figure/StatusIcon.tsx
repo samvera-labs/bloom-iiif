9a63001e526ad2a7a9699c569325c160e5dd50e5
Mat Jordan <mat@northwestern.edu>
2023-01-11 16:27:35 -0500

    Add defensiveness on resource requests.

--- src/components/Figure/StatusIcon.tsx
+++ src/components/Figure/StatusIcon.tsx
@@ -6,13 +6,24 @@ interface StatusProps {
 }
 
 const IconLock = (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    className="icon-lock"
-    viewBox="0 0 512 512"
-  >
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
     <title>Restricted Item</title>
     <path d="M368 192h-16v-80a96 96 0 10-192 0v80h-16a64.07 64.07 0 00-64 64v176a64.07 64.07 0 0064 64h224a64.07 64.07 0 0064-64V256a64.07 64.07 0 00-64-64zm-48 0H192v-80a64 64 0 11128 0z" />
+  </svg>
+);
+
+const IconUnknown = (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+    <title>Unknown Status</title>
+    <path
+      d="M160 164s1.44-33 33.54-59.46C212.6 88.83 235.49 84.28 256 84c18.73-.23 35.47 2.94 45.48 7.82C318.59 100.2 352 120.6 352 164c0 45.67-29.18 66.37-62.35 89.18S248 298.36 248 324"
+      fill="none"
+      stroke="currentColor"
+      stroke-linecap="round"
+      stroke-miterlimit="10"
+      stroke-width="40"
+    />
+    <circle cx="248" cy="399.99" r="32" />
   </svg>
 );
 
@@ -23,6 +34,9 @@ const StatusIcon: React.FC<StatusProps> = ({ status }) => {
     switch (status) {
       case 403:
         setIcon(IconLock);
+        break;
+      default:
+        setIcon(IconUnknown);
         break;
     }
   }, [status]);
--- src/dev.tsx
+++ src/dev.tsx
@@ -4,7 +4,6 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 
 import React from "react";
-import ReactDOM from "react-dom";
 import App from "./index";
 import DynamicUrl from "./dev/DynamicUrl";
 import { collections } from "./dev/collections";
@@ -12,12 +11,6 @@ import { createRoot } from "react-dom/client";
 
 const Wrapper = () => {
   const defaultUrl: string = collections[0].url;
-
-  const oneItem =
-    "https://dcapi.rdc-staging.library.northwestern.edu/api/v2/search?query=subject.label:%22Sculpture,%20English--19th%20century%22&collectionLabel=Sculpture,%20English--19th%20century&collectionSummary=Subject&as=iiif";
-
-  const twoItems =
-    "https://dcapi.rdc-staging.library.northwestern.edu/api/v2/search?query=collection.title.keyword:%22Transportation%20Library%20Menu%20Collection%22&collectionLabel=Transportation%20Library%20Menu%20Collection&collectionSummary=Collection&as=iiif";
 
   const [url, setUrl] = React.useState(defaultUrl);
   return (
